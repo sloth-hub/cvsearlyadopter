11ae7ffc5a7a02fc4a981e98e557afab1db94ddc
sloth-hub <madamhh@gmail.com>
2020-12-28 21:51:16 +0900

    Add firebase SDK and initialize firebase

---
+++ src/routes/Auth.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Auth = () => {
+
+    return (
+        <span>
+            auth
+        </span>
+    );
+
+}
+
+export default Auth;
---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <span>
+            home
+        </span>
+    );
+
+}
+
+export default Home;
