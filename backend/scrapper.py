bdd8736b6df8874fd8724dd807b27fd62aa8d447
sloth-hub <madamhh@gmail.com>
2021-02-01 21:11:21 +0900

    Add CU scraping code to scrapper

--- backend/scrapper.py
+++ backend/scrapper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 
 options = webdriver.ChromeOptions()
-options.add_argument("headless")    # 웹 브라우저를 띄우지 않는 headless chrome 옵션 적용
+# options.add_argument("headless")    # 웹 브라우저를 띄우지 않는 headless chrome 옵션 적용
 options.add_argument("disable-gpu")    # GPU 사용 안함
 options.add_argument("lang=ko_KR")    # 언어 설정
 driver = webdriver.Chrome("./chromedriver", options=options)  # 옵션적용
@@ -13,15 +13,15 @@ driver.execute_script('window.open("about:blank", "_blank");')
 
 tabs = driver.window_handles
 
-# TAB_1
+# TAB_1_GS25
 driver.switch_to_window(tabs[0])
 driver.get("http://gs25.gsretail.com/gscvs/ko/products/youus-freshfood")
  
-# TAB_2
+# TAB_2_7-Eleven
 driver.switch_to_window(tabs[1])
 driver.get("https://7-eleven.co.kr/product/bestdosirakList.asp")
  
-# TAB_3
+# TAB_3_CU
 driver.switch_to_window(tabs[2])
 driver.get("http://cu.bgfretail.com/product/product.do?category=product&depth2=4")
 
@@ -69,4 +69,35 @@ def get_se():
         next_btn.click()
     return se_prods
 
-print(get_se())
+def extract_cu_prod(prod):
+    prod_name = prod.find_element_by_css_selector("p.prodName > span").text
+    prod_price = prod.find_element_by_css_selector("p.prodPrice > span").text
+    prod_image = prod.find_element_by_css_selector("div.photo > a > img").get_attribute("src")
+    return {"name": prod_name, "price": prod_price, "image": prod_image}
+
+def get_cu():
+    cu_prods = []
+    driver.switch_to_window(tabs[2])
+    time.sleep(3)
+    # 최신순 버튼 클릭
+    newest_btn = driver.find_element_by_css_selector("li#setC > span > a")
+    newest_btn.click()
+    for i in range(5):
+        time.sleep(3)
+        try: # 다음 페이지 버튼이 없는 경우 멈춤
+            next_btn = driver.find_element_by_css_selector("div.prodListBtn-w > a")
+            next_btn.click()
+        except:
+            break
+    prod_wrap = driver.find_elements_by_css_selector("div.prodListWrap")
+    for wrap in prod_wrap:
+        prod_list = wrap.find_elements_by_css_selector("ul > li")
+        for prod in prod_list:
+            new = prod.find_elements_by_css_selector("span.tag > img[alt=New]")
+            if new:  # 신상품 표시가 있으면
+                product = extract_cu_prod(prod)
+                cu_prods.append(product)
+        
+    return cu_prods
+
+print(get_cu())
