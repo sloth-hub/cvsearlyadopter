bc1d06020605b8e4e52026ebb5cec7b0cf12b0eb
sloth-hub <madamhh@gmail.com>
2021-02-05 23:57:33 +0900

    Add code that extract only new products

--- backend/firebase.py
+++ backend/firebase.py
@@ -12,8 +12,21 @@ firebase_admin.initialize_app(cred, {
 })
 
 def put_database():
-    ref = db.reference() # 기본 위치 지정
     gs_prods = get_gs()
-    ref.update({"gs" : gs_prods})
+    ref = db.reference() # 기본 위치
+    fb = ref.get()
+    # 데이터베이스에 데이터가 없으면
+    if not fb:
+        ref.update({"gs": gs_prods})
+    else:
+        ref = db.reference("gs") # 기본 위치
+        gs_from_fb = ref.get()
+        # 중복검사하여 새로운 상품만 추출
+        names = {item["name"] for item in gs_from_fb}
+        new_prods = [item for item in gs_prods if item["name"] not in names]
+        if not new_prods:
+            return print("새로운 아이템이 없습니다.")
+        else:
+            return print(new_prods)
 
 put_database()
