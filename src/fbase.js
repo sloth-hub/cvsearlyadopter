5b93732d3cd644aac227dab8d5f7c45bfc4f7003
sloth-hub <madamhh@gmail.com>
2021-02-24 22:59:03 +0900

    Add score and comment to detail page

--- src/fbase.js
+++ src/fbase.js
@@ -1,9 +1,12 @@
 import firebase from "firebase/app";
-import "firebase/database"
+import "firebase/auth";
+import "firebase/database";
+import "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
@@ -12,4 +15,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+export const firebaseInstance = firebase;
 export const database = firebase.database();
+export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
+import { dbService } from "../fbase";
+import moment from "moment";
+import Rating from '@material-ui/lab/Rating';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Detail = () => {
 
     const [prod, setProd] = useState("");
+    const [comment, setComment] = useState("");
+    const [score, setScore] = useState(0);
+
     const location = useLocation();
     const history = useHistory();
 
@@ -16,15 +23,85 @@ const Detail = () => {
         }
     }, [history, location.state]);
 
+    const onChange = (evt) => {
+        const {
+            target: { value }
+        } = evt;
+        setComment(value);
+    }
+
+    const onSubmit = async (evt) => {
+        evt.preventDefault();
+        if (comment !== "") {
+            const commentObj = {
+                text: comment,
+                createdAt: moment().format("YYYY-MM-DD HH:mm:ss"),
+                score: score
+            }
+            await dbService.collection("comment").add(commentObj);
+            setComment("");
+            setScore(0);
+        } else {
+            alert("내용을 입력해주세요.");
+        }
+    }
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+            flexDirection: 'column',
+            '& > * + *': {
+                marginTop: theme.spacing(1),
+            },
+        },
+    }));
+
+    const HalfRating = () => {
+        const classes = useStyles();
+        return (
+            <div className={classes.root} >
+                <Rating name="customized-empty"
+                    defaultValue={0}
+                    value={score}
+                    precision={0.5}
+                    onChange={(evt, newValue) => {
+                        setScore(newValue);
+                    }} />
+            </div >
+        );
+
+    }
+
     return (
-        <>
-            <div>
-                <img src={prod.image} alt={prod.name} />
-                <h4>{prod.name}</h4>
-                <h5>{prod.price}</h5>
-                <span>{prod.score}</span>
+        <div className="main_container">
+            <div className="detail_box">
+                <section className="prod_detail">
+                    <img src={prod.image} alt={prod.name} />
+                    <h4>{prod.name}</h4>
+                    <h5>{prod.price}</h5>
+                    <span>{prod.score}</span>
+                </section>
+                <section className="prod_review">
+                    <form className="commentForm">
+                        <textarea
+                            value={comment}
+                            onChange={onChange}
+                            placeholder="제품이 어떤지 말해주세요."
+                            maxLength={120}
+                            spellCheck="false"
+                            className="comment_input" />
+                        <HalfRating /><span>{score}</span>
+                        <input type="submit" value="등록" />
+                    </form>
+                    <div className="comment_list_wrap">
+                        <ul className="comment_list">
+                            <li>comment</li>
+                            <li>comment</li>
+                        </ul>
+                    </div>
+                </section>
             </div>
-        </>
+        </div>
     );
 
 }
